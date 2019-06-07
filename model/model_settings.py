dc1e221e4f4b13ea36e7cc3cff75e70c270de2ab
Mengting Fang <mtfang@stanford.edu>
2019-06-07 14:11:44 -0400

    Add files via upload

---
+++ model/model_settings.py
@@ -0,0 +1,12 @@
+# model parameters
+
+all_subjects=['sub-01','sub-02','sub-03','sub-04','sub-05','sub-09','sub-10','sub-14','sub-15','sub-16','sub-17','sub-18','sub-19','sub-20']
+#all_subjects=['sub-01','sub-02','sub-03','sub-04','sub-05','sub-09','sub-10','sub-14','sub-15','sub-16']
+all_copes=['cope1_face','cope2_body','cope3_object','cope4_scene']
+sub='sub-18'
+total_run=8
+num_epochs=5000
+save_freq=500
+print_freq=100
+batch_size=32 # or 64
+learning_rate=1e-3
